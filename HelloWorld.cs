09fdbe348aeb5808aec83b26ec63d9a4366662ba
laoguyjack <jsavath@gmail.com>
2018-09-14 10:41:48 -0700

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Jack");
+            Console.WriteLine("Favorite Color: Black");
+            Console.WriteLine("Birthplace: Alaska");
+            Console.Read();
+        }
+    }
+}
